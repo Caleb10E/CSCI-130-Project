7968bd7908458b5b88bc6fe07f00513c79666a0f
Caleb10E <caleb10e@gmail.com>
2018-12-06 23:03:15 -0800

    Beautified the indentation of every file

--- Project/script.js
+++ Project/script.js
@@ -1,14 +1,10 @@
-
-function createTable(tableSize, gridColor, blockColor)
-{
- for(var r = 0; r < parseInt(tableSize.value,10); r++)
-  {
-   var x = document.getElementById('myTable').insertRow(r);
-//   document.getElementById("myTable").style.color = red;
-   for(var c = 0; c < parseInt(tableSize.value,10); c++)
-    {
-     var y = x.insertCell(c);
-     y.innerHTML= "     ";
+function createTable(tableSize, gridColor, blockColor) {
+  for(var r = 0; r < parseInt(tableSize.value,10); r++) {
+    var x = document.getElementById('myTable').insertRow(r);
+    // document.getElementById("myTable").style.color = red;
+    for(var c = 0; c < parseInt(tableSize.value,10); c++) {
+      var y = x.insertCell(c);
+      y.innerHTML= "     ";
     }
-   }
+  }
 }
