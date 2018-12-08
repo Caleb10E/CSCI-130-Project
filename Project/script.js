38d4b28bb6b06ec402b7dc9c60f07672d8a03eca
Troy Samra <tesamra@gmail.com>
2018-12-07 16:39:29 -0800

    fixed block colors

--- Project/script.js
+++ Project/script.js
@@ -1,27 +1,34 @@
 function createTable(tableSize, gridColor, blockColor) {
-    document.getElementById('myTable').style.textAlign='center';
   for(var r = 0; r < parseInt(tableSize.value,10); r++) {
     var x = document.getElementById('myTable').insertRow(r);
     for(var c = 0; c < parseInt(tableSize.value,10); c++) {
       var but = document.createElement('input');
       but.setAttribute('type', 'button');
       but.setAttribute('name','javaSubmit');
-      but.setAttribute('value','');
-      but.setAttribute('onclick', 'alertHello()');
+      but.setAttribute('id','uniqueIdentifier');
+      but.setAttribute('value',' ');
+      but.setAttribute('onmousedown', 'WhichButton(event)');
       but.style.width = '50px';
       but.style.height = '50px';
+      but.style.backgroundColor = blockColor.value;
         var y = x.insertCell(c);
         y.appendChild(but);
-    //  y.innerHTML= r+ c;
-    //  y.style.color = gridColor;
     }
   }
 }
 
-function alertHello() {
-  alert('Hello World');
-}
-
 function WhichButton(event) {
-    alert("You pressed button: " + event.button)
+  if(event.button == 0){
+  alert("You pressed button: " + event.button);
+  }
+
+  else if(event.button == 2) {
+      alert("You pressed button: " + event.button);
+    // make button an X
+    var elem = document.getElementById("uniqueIdentifier");
+    elem.value = "X";
+  }
+ else {
+     alert("Invalid Input");
+ }
 }
